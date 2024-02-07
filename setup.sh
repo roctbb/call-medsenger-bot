5de899c6ce11cda79c3830644864e0b2662220f8
roctbb <roctbb@gmail.com>
2024-02-07 14:35:48 +0300

    prepare for emperor

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo pip3 install -r requirements.txt
 sudo cp agents_call.conf /etc/supervisor/conf.d/
 sudo cp agents_call_nginx.conf /etc/nginx/sites-enabled/
+sudo cp call.ini /etc/uwsgi/apps/
 sudo supervisorctl update
 sudo systemctl restart nginx
 sudo certbot --nginx -d call.ai.medsenger.ru
